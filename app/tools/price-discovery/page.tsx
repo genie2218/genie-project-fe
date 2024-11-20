e9c776ca3493c8104692a7bf1ee5d1a6c19990a2
Arpit Rathi <arpit97rathi@gmail.com>
2024-11-21 01:23:16 +0530

    feat: initial commit

---
+++ app/tools/price-discovery/page.tsx
@@ -0,0 +1,11 @@
+import styles from './styles.module.scss';
+
+const PriceDiscovery = () => {
+  return (
+    <div className={styles.container}>
+      <p>Price Discovery</p>
+    </div>
+  )
+}
+
+export default PriceDiscovery;
